89ff8fcc6cd2063ad3d144ffdf80db4b88080318
Lucas <lucas.lucenak@gmail.com>
2021-10-16 00:21:15 -0300

    nothin

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -14,7 +14,7 @@ function Dashboard() {
 
                     <div className="row px-3" >
                         <div className="col-sm-6">
-                            <h5 className="text-center text-secondary"> Taxa de Sucesso (%) </h5>
+                            <h5 className="text-center text-secondary"> MICHELE SUA LINDA (%) </h5>
                             <BarChart />
                         </div>
                         <div className="col-sm-6">
