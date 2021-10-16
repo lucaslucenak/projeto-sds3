d6f3187dedc83ad103129bbdcca10f74570e7a94
Lucas <lucas.lucenak@gmail.com>
2021-10-16 07:36:29 -0300

    back to original

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -10,20 +10,20 @@ function Dashboard() {
             <>
                 <NavBar />
                 <div className="container">
-                    <h1 className="text-primary py-3">MICHELE SUA LINDA</h1>
+                    <h1 className="text-primary py-3">Dashboard de vendas</h1>
 
                     <div className="row px-3" >
                         <div className="col-sm-6">
-                            <h5 className="text-center text-secondary"> MICHELE SUA LINDA (%) </h5>
+                            <h5 className="text-center text-secondary"> Taxa de Sucesso (%) </h5>
                             <BarChart />
                         </div>
                         <div className="col-sm-6">
-                            <h5 className="text-center text-secondary"> MICHELE SUA LINDA </h5>
+                            <h5 className="text-center text-secondary"> Todas as vendas </h5>
                             <DonutChart />
                         </div>
                     </div>
                     <div className="py-3">
-                        <h2 className="text-primary"> MICHELE SUA LINDA</h2>
+                        <h2 className="text-primary"> Todas as vendas</h2>
                     </div>
                     <DataTable />
                 </div>
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -8,16 +8,13 @@ function Home() {
             <NavBar />
             <div className="container">
                 <div className="jumbotron">
-                    <h1 className="display-4">oi gatinha, você eh muito linda viu?</h1>
+                    <h1 className="display-4">DSVendas</h1>
                     <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
                     <hr />
                     <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
-                    <p><img src="coracao.png" alt = "michele muito gata do sorriso lindo"/></p>
                     <Link className="btn btn-primary btn-log" to="/dashboard">
                         Acessar dashboard
-                        <p><img src="coracao.png" alt = "michele muito gata do sorriso lindo"/></p>
                     </Link>
-                    <p><img src="coracao.png" alt = "michele muito gata do sorriso lindo"/></p>
                 </div>
             </div>
             <Footer />
